04d295488f585f33f5359c162231fdbd7b16442f
Alex Wang <y2587wan@edu.uwaterloo.ca>
2018-08-24 23:22:18 -0400

    Basic working version for stop sign and school zone sign

---
+++ AR Sign Recognizer/Assets/Scripts/TextFollow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextFollow : MonoBehaviour {
+
+
+    public GameObject targetObject;
+    public Camera cameraAR;
+    private Vector3 targetObjectPos;
+    private RectTransform rt;
+    private RectTransform canvasRT;
+    private Vector3 targetObjectScreenPos;
+
+    // Use this for initialization
+    void Start()
+    {
+        targetObjectPos = targetObject.transform.position;
+
+        rt = GetComponent<RectTransform>();
+        canvasRT = GetComponentInParent<Canvas>().GetComponent<RectTransform>();
+        targetObjectScreenPos = cameraAR.WorldToViewportPoint(targetObject.transform.TransformPoint(targetObjectPos));
+        rt.anchorMax = targetObjectScreenPos;
+        rt.anchorMin = targetObjectScreenPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetObjectScreenPos = cameraAR.WorldToViewportPoint(targetObject.transform.TransformPoint(targetObjectPos));
+        rt.anchorMax = targetObjectScreenPos;
+        rt.anchorMin = targetObjectScreenPos;
+    }
+}
---
+++ AR Sign Recognizer/Assets/Scripts/VisibleText.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisibleText : MonoBehaviour {
+    BoxCollider selfCollider;
+	void Start () {
+        selfCollider = GetComponent<BoxCollider>();
+        var child = transform.GetChild(0);
+        child.gameObject.SetActive(selfCollider.enabled);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        var child = transform.GetChild(0);
+        child.gameObject.SetActive(selfCollider.enabled);
+    }
+}
