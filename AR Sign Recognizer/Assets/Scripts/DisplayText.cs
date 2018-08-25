24820907ec18a80bf0357de9453d602b51b73b92
Alex Wang <y2587wan@edu.uwaterloo.ca>
2018-08-25 05:10:54 -0400

    Stable version for sign reconizer

---
+++ AR Sign Recognizer/Assets/Scripts/DisplayText.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayText : MonoBehaviour {
+    [SerializeField]
+    private Text text;
+    private void Start()
+    {
+        text.text = gameObject.name;
+    }
+}
